d06bd50f654eb8190de3db0d824cd2620b95f8b9
claudiobak <claudiobakker@gmail.com>
2022-03-08 12:11:13 +0100

    changed action in loginform

---
+++ static/script.js
@@ -0,0 +1,18 @@
+// const btn = document.querySelector('#btn');
+// let form = document.querySelector('#loginform');
+
+// form.addEventListener('submit', check);
+
+// function check (event) {
+// 	event.preventDefault()
+// 	let terms = document.querySelector('#terms').checked;
+// 	console.log(terms)
+	
+// 	if (terms == false){
+// 		btn.classList.add("checked")
+// 	}
+// 	else{
+// 		form.submit()
+// 	}
+	
+// }
