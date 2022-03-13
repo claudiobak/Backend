4529961d2643a4c8e1ca88081e261c81947f4718
claudiobak <claudiobakker@gmail.com>
2022-03-13 16:42:04 +0100

    added user schema

--- server.js
+++ server.js
@@ -1,42 +1,7 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
 
 
-
-// const express =  require ('express')
-
-// express()
-//   .use('/static',express.static('static'))
-
-//   .get('/', onhome)
-//   .get('/about', onabout)
-//   .get('/login', onlogin)
-
-//   .listen(8000)
-
-// function onhome(req, res) {
-//   res.send('<h1>Hello World!</h1>')
-// }
-
-// function onabout(req, res) {
-//   res.send('<h1>About me</h1>')
-// }
-
-// function onlogin(req, res) {
-//   res.send('<h1>Login page</h1>')
-// }
-
-require('dotenv').config()
-console.log(process.env) // remove this after you've confirmed it working
+// require('dotenv').config()
+// console.log(process.env) // remove this after you've confirmed it working
 
 const express = require('express')
 const res = require('express/lib/response')
@@ -44,17 +9,17 @@ const app =  express()
 const PORT = process.env.PORT || 1337
 const bodyParser = require('body-parser')
 
-const Tweet = require('./modals/Tweet')
+// const Tweet = require('./modals/Tweet')
 
 require('dotenv').config()
 const connectDB = require('./config/db')
 
 connectDB();
 
-app.post('/createTweet', async (req, res) => {
-  const tweet = new Tweet (req.body)
-  await tweet.save()
-})
+// app.post('/createTweet', async (req, res) => {
+//   const tweet = new Tweet (req.body)
+//   await tweet.save()
+// })
 
 const { engine } = require ('express-handlebars');
 app.engine('hbs', engine({
@@ -70,10 +35,6 @@ app.set('views', 'view');
 
 app.use(express.urlencoded({extended:false}))
 
-
-// app.get('/', (req, res) => {
-//   res.send('login.hbs')
-// })
 
 app.get('/', (req, res) => {
   res.render('startscreen.hbs')
@@ -97,7 +58,7 @@ app.get('/contact', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.send('<h1>Not found..</h1>')
+  res.render('error.hbs')
 })
 
 app.post('/signup', (req, res) => {
@@ -107,3 +68,52 @@ app.post('/signup', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Hello on port: ${PORT}`)
 })
+
+
+// //////////////////
+//     new try     //
+// //////////////////
+
+
+
+
+app.post("/test-post", async (req, res) => {
+  await database
+    .collection("test_collection")
+    .insertOne({ name: req.body.name, age: req.body.age });
+
+  let newUser = await database
+    .collection("test_collection")
+    .findOne({ name: req.body.name });
+
+  console.log(newUser); // Log the new user to the database
+
+  res.redirect("/");
+});
+
+const username = "Stein";
+const password = "SuperCoolWachtwoord";
+
+let session;
+
+app.post("/login-test", async (req, res) => {
+  console.log(req.body);
+  if (req.body.username == username && req.body.password == password) {
+    // Check if the username and password are correct
+    console.log("Valid username and password");
+    session = req.session;
+    session.username = req.body.username;
+    console.log(req.session);
+    res.redirect("/login");
+  } else {
+    console.log("Invalid username or password");
+    res.redirect("/");
+  }
+});
+
+app.get("/login", (req, res) => {
+  console.log(req.session.username); // Get the username from the session
+
+  res.render("login", { username: req.session.username }); // Render the login page
+});
+
