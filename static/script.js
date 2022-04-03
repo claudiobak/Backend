8ba754138bfa35c9500ec8632a8bc213d8a02627
Claudio Bakker <claudiobakker@gmail.com>
2022-04-03 22:35:46 +0200

    fixed error message on register

--- static/script.js
+++ static/script.js
@@ -28,7 +28,7 @@ signupForm.addEventListener("submit", (event) => {
 	const errorLabel = event.target.querySelector("label#error");
 	// als de inputs geen value hebben maakt hij een class aan genaamd errorLabel en zet hij hier tekst in.
 	if (!inputs[0].value || !inputs[1].value) {
-        errorLabel.classList.add("errorLabel2");
+        errorLabel.classList.add("errorLabel");
 		errorLabel.innerHTML = "<span>Error:</span> Please fill in all fields!";
 		inputs.forEach(element => {
 			element.classList.add("fouteInput");
